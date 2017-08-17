f3d8429d148a71b472e9849be38520986a48f00d
Takahiko Sato <takabow@gmail.com>
2017-08-17 12:28:38 +0900

    Modify some typos

--- common/bootstrap-common.sh
+++ common/bootstrap-common.sh
@@ -12,6 +12,8 @@ ln -sf  /usr/share/zoneinfo/Japan /etc/localtime
 # JDK8 installation
 yum remove --assumeyes *openjdk*
 rpm -ivh "http://archive.cloudera.com/director/redhat/7/x86_64/director/2.5.0/RPMS/x86_64/oracle-j2sdk1.8-1.8.0+update121-1.x86_64.rpm"
-alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_121-cloudera/bin/java 1
+JAVA_HOME="/usr/java/jdk1.8.0_121-cloudera"
+alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
+alternatives --install /usr/bin/javac javac ${JAVA_HOME}/bin/javac 1
 ln -nfs /usr/java/jdk1.8.0_121-cloudera /usr/java/latest
 ln -nfs /usr/java/latest /usr/java/default
