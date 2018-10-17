75f56be3bb6d01be4e8b1be17dae0c53101e0288
Takahiko Sato <takabow@gmail.com>
2018-10-17 11:10:46 +0900

    Add High Abailability to C5 conf also

--- scripts/common/bootstrap-common.sh
+++ scripts/common/bootstrap-common.sh
@@ -11,12 +11,7 @@ ln -sf  /usr/share/zoneinfo/Japan /etc/localtime
 
 # JDK8 installation
 yum remove --assumeyes *openjdk*
-#rpm -ivh "http://archive.cloudera.com/director/redhat/7/x86_64/director/2.8.0/RPMS/x86_64/oracle-j2sdk1.8-1.8.0+update121-1.x86_64.rpm"
-#JAVA_HOME="oracle-j2sdk1.8-1.8.0+update121-1.x86_64.rpm"
-#alternatives --install /usr/bin/java java ${JAVA_HOME}/bin/java 1
-#alternatives --install /usr/bin/javac javac ${JAVA_HOME}/bin/javac 1
-#ln -nfs /usr/java/jdk1.8.0_121-cloudera /usr/java/latest
-#ln -nfs /usr/java/latest /usr/java/default
+rpm -ivh "http://archive.cloudera.com/director6/6.0.0/redhat7/RPMS/x86_64/oracle-j2sdk1.8-1.8.0+update141-1.x86_64.rpm"
 
 #Use ntpd instead of chrony
 #And Use Amazon Time Sync Service mainly - https://docs.aws.amazon.com/ja_jp/AWSEC2/latest/UserGuide/set-time.html
